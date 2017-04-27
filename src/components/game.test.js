af506fd13f425c9d20ab265c7d8353e985f76f59
Matthew Long <MattLong87@gmail.com>
2017-04-27 16:03:22 -0400

    added state checking when guess is submitted

--- src/components/game.test.js
+++ src/components/game.test.js
@@ -8,4 +8,11 @@ describe('<Game />', () => {
 		shallow(<Game />);
 	})
 
+	it('Adds a guess to state when submitted', () => {
+		const testGuess = 33;
+		const wrapper = mount(<Game />);
+		wrapper.find('input').node.value = testGuess;
+		wrapper.find('form').simulate('submit');
+		expect(wrapper.state('guesses')).toEqual([testGuess])
+	})
 })
